a6eefac1f790fb1ba0bce97388734f1cc1253296
chenyang <hchenyang@hotmail.com>
2019-09-15 20:37:56 -0600

    drawing curve

---
+++ draw.py
@@ -0,0 +1,17 @@
+import pickle as pkl
+import matplotlib.pyplot as plt
+
+with open('train_val_loss.pkl', 'br') as f:
+    train_val_loss_list = pkl.load(f)
+
+train_loss_list = [x[0] for x in train_val_loss_list]
+val_loss_list = [x[1] for x in train_val_loss_list]
+
+x = [x + 1 for x in list(range(len(train_val_loss_list)))]
+
+plt.plot(x, train_loss_list, 'r', label='Train')
+plt.plot(x, val_loss_list, 'b', label='Val')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend()
+plt.show()
